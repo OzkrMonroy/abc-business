b453658ab68dcfc71c2e2ff9d93f584b92c0b3ad
Oscar Monroy <oscarmonroy657@gmail.com>
2021-07-02 09:24:04 -0600

    Configuration for firebase hosting is ready.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,21 +1,17 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { signOutStartAsync } from '../../redux/user/userAction';
 import { DrawerNav } from '../drawer/DrawerNav';
 import { Header } from './header/Header';
 import { LayoutContainer, MainContainer } from './layoutStyles';
 
 export const Layout = ({children}: any) => {
   const [openDrawer, setOpenDrawer] = useState(false);
-  const dispatch = useDispatch();
 
   const closeDrawer = () => {
     setOpenDrawer(false);
   }
 
   const openDrawerHandler = () => {
-    // setOpenDrawer(true);
-    dispatch(signOutStartAsync());
+    setOpenDrawer(true);
   }
 
   return (
